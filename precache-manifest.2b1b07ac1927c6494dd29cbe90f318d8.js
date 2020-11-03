474076d3dd3cdb3464a8aaa4eb5afb4725a084c8
sunweiqi <921939025@qq.com>
2020-11-03 18:20:10 +0800

    Init

---
+++ precache-manifest.2b1b07ac1927c6494dd29cbe90f318d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00f718769e4c2f8026666ab3d6d551d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b934e25b5ac54a3521d",
+    "url": "/static/css/2.eb7e7087.chunk.css"
+  },
+  {
+    "revision": "94891a3ff135ade30ca5",
+    "url": "/static/css/main.f135b8e2.chunk.css"
+  },
+  {
+    "revision": "7b934e25b5ac54a3521d",
+    "url": "/static/js/2.230cdce5.chunk.js"
+  },
+  {
+    "revision": "e76d22c6b3046b653cdc5c6ad5ab0e8e",
+    "url": "/static/js/2.230cdce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94891a3ff135ade30ca5",
+    "url": "/static/js/main.bd2efc71.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
