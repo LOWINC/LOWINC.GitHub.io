2ade00246131df5ebb3fea57b38ba39940e6e42e
sunweiqi <921939025@qq.com>
2020-11-11 15:14:14 +0800

    mobile

---
+++ mobile/precache-manifest.dd59cfe279ae5a9e5cc9ced52a0835f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2677b27d6a5ac38fdb2fb9927e1f7e9",
+    "url": "/mobile/index.html"
+  },
+  {
+    "revision": "bc0dc5f01e0113e798c9",
+    "url": "/mobile/static/js/2.1e9e8658.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mobile/static/js/2.1e9e8658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193f01a6ebc3eb35119b",
+    "url": "/mobile/static/js/main.9fed586c.chunk.js"
+  },
+  {
+    "revision": "982a9ab110cdc058fb03",
+    "url": "/mobile/static/js/runtime-main.1431d30f.js"
+  }
+]);
